e8e668202091ad99c8f308371b649b251ec81a47
Edward L Platt <ed@elplatt.com>
2014-02-05 19:10:41 -0500

    Add controls drop down menu.

--- app/static/views.js
+++ app/static/views.js
@@ -9,16 +9,21 @@ App.AppView = Backbone.View.extend({
         this.options = options || {};
         this.userModel = options.userModel;
         _.bindAll(this, 'render');
+        // Create models
         options.userModel.on('change', this.render);
+        // Create sub-views
         this.loginView = new App.LoginView({ model: options.userModel });
+        this.controlsView = new App.ControlsView({ userModel: options.userModel });
         this.render();
     },
     
     render: function () {
+        this.$el.html('');
+        $(this.el).append(this.controlsView.el);
         if (this.options.userModel.get('authenticated')) {
-            $(this.el).html('Hello, ' + this.userModel.get('username'))
+            $(this.el).append('Hello, ' + this.userModel.get('username'))
         } else {
-            $(this.el).html(this.loginView.el);
+            $(this.el).append(this.loginView.el);
         }
         return this;
     }
@@ -55,13 +60,50 @@ App.LoginView = Backbone.View.extend({
         password = $('input[name=password]', this.$el).val();
         this.model.fetch({
             type: 'post',
-            data: {'username': username, 'password': password},
-            success: function (model, response, options) {
-                App.debug('Signed in as user: ' + model.get('username'));
-            },
-            error: function (model, response, options) {
-                App.debug('Error signing in');
-            }
+            data: {'username': username, 'password': password}
         });
     }
 });
+
+/**
+ * Controls drop-down menu
+ */
+App.ControlsView = Backbone.View.extend({
+    
+    template: _.template($('#tpl-controls-view').html()),
+    
+    initialize: function (options) {
+        this.options = options || {};
+        this.userModel = options.userModel;
+        _.bindAll(this, 'render');
+        this.userModel.on('change', this.render);
+        this.controlsSignOutView = new App.ControlsSignOutView({ userModel: this.userModel })
+        this.render();
+    },
+    
+    render: function () {
+        this.$el.addClass('controls');
+        this.$el.html(this.template());
+        if (this.userModel.get('authenticated')) {
+            console.log('authed');
+            $('ul', this.$el).append(this.controlsSignOutView.el);
+        } else {
+            console.log('not authed');
+        }
+        return this;
+    }
+});
+
+App.ControlsSignOutView = Backbone.View.extend({
+    tagName: 'li',
+    template: _.template($('#tpl-controls-sign-out-view').html()),
+    initialize: function (options) {
+        this.render();
+    },
+    render: function () {
+        App.debug('rendering ControlsSignOutView');
+        this.$el.html(this.template())
+        return this;
+    }
+})
+
