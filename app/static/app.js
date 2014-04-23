7ffc837305717135cffeb8ea860fa7dd9fabcf43
Edward L Platt <ed@elplatt.com>
2014-04-23 13:11:54 -0400

    Update colors on attention plotter for #64.

--- app/static/app.js
+++ app/static/app.js
@@ -6,7 +6,8 @@ App = {
             format: 'yyyy-mm-dd'
         },
         fullMonthNames: [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ],
-        shortMonthNames: [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec" ]
+        shortMonthNames: [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec" ],
+        queryColors: ['#e14c11', "#249fc9"]
     },
     
     initialize: function () {
