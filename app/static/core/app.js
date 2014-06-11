80244d2f35c5dbe8d1fc464d2df934fcf42ed09f
Edward L Platt <ed@elplatt.com>
2014-06-11 14:46:10 -0400

    Separate config/util in App from Controller functionality.

--- app/static/core/app.js
+++ app/static/core/app.js
@@ -10,53 +10,6 @@ App = {
         queryColors: ['#e14c11', "#249fc9"]
     },
     
-    initialize: function () {
-        App.debug('App.initialize()');
-        App.instance = this;
-        // Create models and router
-        this.userModel = new App.UserModel();
-        this.mediaSources = new App.MediaModel({parse:true});
-        this.router = new App.Router({
-            userModel: this.userModel
-            , mediaSources: this.mediaSources
-        });
-        // Bind event handlers
-        _.bindAll(this, 'onSignIn');
-        _.bindAll(this, 'onSignOut');
-        // Listener for events
-        this.userModel.on('signin', this.onSignIn);
-        this.userModel.on('signout', this.onSignOut);
-        this.userModel.on('unauthorized', this.onSignOut);
-        // Start navigation and log user in
-        this.userModel.fetch({
-            "type": "post"
-            , "success": function() { _.defer(function () { Backbone.history.start(); }); }
-            , "error": function() { _.defer(function () { Backbone.history.start(); }); }
-        });
-    },
-    
-    onSignIn: function () {
-        App.debug('App.onSignIn()');
-        var that = this;
-        if (this.mediaSources.get('sources').length == 0) {
-            $.ajax('/static/core/data/media.json', {
-                "dataType": "json",
-                "success": function (data) {
-                    App.debug('Received media json:');
-                    that.mediaSources.set(that.mediaSources.parse(data));
-                    that.mediaSources.trigger('sync');
-                    that.mediaSourceData = data;
-                }
-            })
-        }
-        this.router.navigate('', true);
-    },
-    
-    onSignOut: function () {
-        App.debug('App.onSignOut()');
-        this.router.navigate('login', true);
-    },
-    
     // Take a Collection and return a map using the specified key
     makeMap: function (col, key) {
         dataMap = {};
