d2395918736d9567764e92b4cf8e72d70d9bde08
Rahul Bhargava <rahulb@media.mit.edu>
2014-06-16 15:31:51 -0400

    fixed namespacing so that loading from query URL works now

--- app/dashboard/static/controller.js
+++ app/dashboard/static/controller.js
@@ -4,13 +4,13 @@ _.extend(App.Controller, {
     showResults: function (queryCollection) {
         // Create new results view
         App.debug("App.Controller.showResults")
-        var resultView = this.vm.getView(
+        var resultView = App.con.vm.getView(
             App.QueryResultView,
             { collection: queryCollection },
             true
         );
-        this.vm.showViews([
-            this.queryListView
+        App.con.vm.showViews([
+            App.con.queryListView
             , resultView
         ]);
         resultView.render();
@@ -19,11 +19,10 @@ _.extend(App.Controller, {
     
     routeDemoQuery: function (keywords, media, start, end) {
         App.debug('Route: demoQuery ------------------------------------------------------------------------------------------------------------------------');
-        var that = this;
         // Defaults media
-        this.mediaSources = new App.MediaModel();
-        this.mediaSources.set(
-            this.mediaSources.parse({
+        App.con.mediaSources = new App.MediaModel();
+        App.con.mediaSources.set(
+            App.con.mediaSources.parse({
                 'sources': [{"media_id":1,"url":"http://nytimes.com","name":"New York Times"},{"media_id":2,"url":"http://washingtonpost.com","name":"Washington Post"},{"media_id":4,"url":"http://www.usatoday.com","name":"USA Today"},{"media_id":6,"url":"http://www.latimes.com/","name":"LA Times"},{"media_id":7,"url":"http://www.nypost.com/","name":"The New York Post"},{"media_id":8,"url":"http://www.nydailynews.com/","name":"The Daily News New York"},{"media_id":14,"url":"http://www.sfgate.com/","name":"San Francisco Chronicle"},{"media_id":314,"url":"http://www.huffingtonpost.com/","name":"The Huffington Post"},{"media_id":1089,"url":"http://www.reuters.com/","name":"Reuters"},{"media_id":1092,"url":"http://www.foxnews.com/","name":"FOX News"},{"media_id":1094,"url":"http://www.bbc.co.uk/?ok","name":"BBC"},{"media_id":1095,"url":"http://www.cnn.com/","name":"CNN"},{"media_id":1098,"url":"http://www.newsweek.com/","name":"Newsweek "},{"media_id":1104,"url":"http://www.forbes.com/","name":"Forbes"},{"media_id":1149,"url":"http://www.msnbc.msn.com/","name":"MSNBC"},{"media_id":1747,"url":"http://www.dailymail.co.uk/home/index.html","name":"Daily Mail"},{"media_id":1750,"url":"http://www.telegraph.co.uk/","name":"Daily Telegraph"},{"media_id":1751,"url":"http://www.guardian.co.uk/","name":"Guardian"},{"media_id":1752,"url":"http://www.cbsnews.com/","name":"CBS News"},{"media_id":4415,"url":"http://cnet.com","name":"CNET"},{"media_id":4418,"url":"http://examiner.com","name":"Examiner.com"},{"media_id":4419,"url":"http://time.com","name":"TIME.com"}]
                 , 'tag_sets' : [
                     {
@@ -43,18 +42,18 @@ _.extend(App.Controller, {
                 ]
             })
         );
-        this.mediaSources.trigger('sync');
+        App.con.mediaSources.trigger('sync');
         keywordList = $.parseJSON(keywords);
         startList = $.parseJSON(start);
         endList = $.parseJSON(end);
         // Create query collection
-        if (!this.queryCollection) {
-            this.queryCollection = new App.QueryCollection();
+        if (!App.con.queryCollection) {
+            App.con.queryCollection = new App.QueryCollection();
         } else {
-            this.queryCollection.reset();
+            App.con.queryCollection.reset();
         }
         // When sources are loaded, populate the media models from the url
-        this.mediaSources.deferred.then(function() {
+        App.con.mediaSources.deferred.then(function() {
             // Add a media model for each query
             // TODO this should really happen in MediaCollection/MediaModel
             _.each($.parseJSON(media), function (d, i) {
@@ -65,12 +64,12 @@ _.extend(App.Controller, {
                     , start: startList[i]
                     , end: endList[i]
                 }, {
-                    mediaSources: that.mediaSources
+                    mediaSources: App.con.mediaSources
                     , parse: true
                     , ResultModel: App.DemoResultModel
                 });
-                that.queryCollection.add(queryModel);
-                var subset = that.mediaSources.subset(d);
+                App.con.queryCollection.add(queryModel);
+                var subset = App.con.mediaSources.subset(d);
                 subset.get('sources').each(function (m) {
                     mediaModel.get('sources').add(m);
                 });
@@ -81,38 +80,37 @@ _.extend(App.Controller, {
                     mediaModel.get('tag_sets').add(m);
                 });
             });
-            that.queryCollection.execute();
+            App.con.queryCollection.execute();
         });
-        this.queryListView = this.vm.getView(
+        App.con.queryListView = App.con.vm.getView(
             App.DemoQueryListView
             , {
-                collection: this.queryCollection
-                , mediaSources: this.mediaSources
+                collection: App.con.queryCollection
+                , mediaSources: App.con.mediaSources
             }
         );
-        this.queryCollection.on('execute', this.onDemoQuery, this);
-        this.queryCollection.on('add', this.onQueryAdd, this);
-        this.showResults(this.queryCollection);
+        App.con.queryCollection.on('execute', App.con.onDemoQuery, this);
+        App.con.queryCollection.on('add', App.con.onQueryAdd, this);
+        App.con.showResults(App.con.queryCollection);
     },
     
     routeQuery: function (keywords, media, start, end) {
         App.debug('Route: query ------------------------------------------------------------------------------------------------------------------------');
-        var that = this;
         keywordList = $.parseJSON(keywords);
         startList = $.parseJSON(start);
         endList = $.parseJSON(end);
-        if (!this.userModel.get('authenticated')) {
-            this.navigate('login', true);
+        if (!App.con.userModel.get('authenticated')) {
+            App.con.navigate('login', true);
             return;
         }
         // Create query collection
-        if (!this.queryCollection) {
-            this.queryCollection = new App.QueryCollection();
+        if (!App.con.queryCollection) {
+            App.con.queryCollection = new App.QueryCollection();
         } else {
-            this.queryCollection.reset();
+            App.con.queryCollection.reset();
         }
         // When sources are loaded, populate the media models from the url
-        this.mediaSources.deferred.then(function() {
+        App.con.mediaSources.deferred.then(function() {
             // Add a media model for each query
             // TODO this should really happen in MediaCollection/MediaModel
             _.each($.parseJSON(media), function (d, i) {
@@ -123,11 +121,11 @@ _.extend(App.Controller, {
                     , start: startList[i]
                     , end: endList[i]
                 }, {
-                    mediaSources: that.mediaSources
+                    mediaSources: App.con.mediaSources
                     , parse: true
                 });
-                that.queryCollection.add(queryModel);
-                var subset = that.mediaSources.subset(d);
+                App.con.queryCollection.add(queryModel);
+                var subset = App.con.mediaSources.subset(d);
                 subset.get('sources').each(function (m) {
                     mediaModel.get('sources').add(m);
                 });
@@ -138,26 +136,25 @@ _.extend(App.Controller, {
                     mediaModel.get('tag_sets').add(m);
                 });
             });
-            that.queryCollection.execute();
+            App.con.queryCollection.execute();
         });
-        this.queryListView = this.vm.getView(
+        App.con.queryListView = App.con.vm.getView(
             App.QueryListView
             , {
-                collection: this.queryCollection
-                , mediaSources: this.mediaSources
+                collection: App.con.queryCollection
+                , mediaSources: App.con.mediaSources
             }
         );
-        this.queryCollection.on('execute', this.onQuery, this);
-        this.queryCollection.on('add', this.onQueryAdd, this);
-        this.showResults(this.queryCollection);
+        App.con.queryCollection.on('execute', App.con.onQuery, this);
+        App.con.queryCollection.on('add', App.con.onQueryAdd, this);
+        App.con.showResults(App.con.queryCollection);
     },
     
     routeDebugHistogram: function () {
         App.debug('Route: query');
-        var that = this;
         // Create query collection and add a model
         var opts = {
-            mediaSources: this.mediaSources
+            mediaSources: App.con.mediaSources
         };
         var queryCollection = new App.QueryCollection([], opts);
         console.log(queryCollection.length);
@@ -169,17 +166,16 @@ _.extend(App.Controller, {
         datecounts.fetch({ parse:true });
         console.log(queryCollection.length);
         var histogramView = new App.HistogramView({collection:queryCollection});
-        this.vm.showViews([
+        App.con.vm.showViews([
             histogramView
         ]);
     },
 
     routeDebugWordCount: function () {
         App.debug('Route: query');
-        var that = this;
         // Create query collection and add a model
         var opts = {
-            mediaSources: this.mediaSources
+            mediaSources: App.con.mediaSources
         };
         var queryCollection = new App.QueryCollection({}, opts);
         var queryModel = new App.QueryModel({}, opts);
@@ -193,7 +189,7 @@ _.extend(App.Controller, {
             , success:function (collection) { console.log(collection); }
         });
         var wordCountView = new App.DebugWordCountView({collection:queryCollection});
-        this.vm.showViews([
+        App.con.vm.showViews([
             wordCountView
         ]);
     },
@@ -201,12 +197,11 @@ _.extend(App.Controller, {
     // First pass at Comparison word cloud, based on static json data
     routeDebugWordCountComparison: function () {
         App.debug('Route: query');
-        var that = this;
         // Create query collection and add a model
         var opts = {
-            mediaSources: this.mediaSources,
-            query1Words: this.query1Words,
-            query2Words: this.query2Words
+            mediaSources: App.con.mediaSources,
+            query1Words: App.con.query1Words,
+            query2Words: App.con.query2Words
         };
 
         var queryModel = new App.QueryModel({}, opts);
@@ -231,7 +226,7 @@ _.extend(App.Controller, {
 
         var wordCountView = new App.WordCountComparisonView({collection:queryCollection
         });
-        this.vm.showViews([
+        App.con.vm.showViews([
             wordCountView
         ]);
     },
--- app/dashboard/static/routes.js
+++ app/dashboard/static/routes.js
@@ -1,8 +1,7 @@
 
 _.extend(App.Router.prototype.routes, {
-    'demo': App.Controller.routeDemo
-    , 'demo-query': App.Controller.routeDemoQuery
-    , 'query': App.Controller.routeQuery
+    'demo-query/:keywords/:media/:start/:end': App.Controller.routeDemoQuery
+    , 'query/:keywords/:media/:start/:end': App.Controller.routeQuery
     , 'debug/histogram': App.Controller.routeDebugHistogram
     , 'debug/wordCount': App.Controller.debugWordCount
     , 'debug/wordCountComparison': App.Controller.debugWordCountComparison
