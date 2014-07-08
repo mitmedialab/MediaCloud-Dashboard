104c998cc25d9320507e686d9638fdd954998e9f
Rahul Bhargava <rahulb@media.mit.edu>
2014-07-08 11:09:06 -0400

    return errors to client if API call fails (more work on #98)

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -13,16 +13,22 @@ _wordcount_export_props = ['term','stem','count']
 @flapp.route('/api/demo/sentences/numfound/<keywords>/csv')
 def demo_sentence_numfound_csv(keywords):
     media, start, end = app.core.views.demo_params()
-    results = json.loads(app.core.views._sentence_numfound(mc_key, keywords, media, start, end))
-    return _assemble_csv_response(results, _sentence_export_props, _sentence_export_columns,'demo-sentence-counts')
+    try:
+        results = json.loads(app.core.views._sentence_numfound(mc_key, keywords, media, start, end))
+        return _assemble_csv_response(results, _sentence_export_props, _sentence_export_columns,'demo-sentence-counts')
+    except Exception as exception:
+        return json.dumps({'error':str(exception)}, separators=(',',':')), 400
 
 @flapp.route('/api/sentences/numfound/<keywords>/<media>/<start>/<end>/csv')
 @flask_login.login_required
 def sentence_numfound_csv(keywords, media, start, end):
     api_key = flask_login.current_user.get_id()
     app.core.logger.debug("sentence_numfound_csv: %s %s %s %s", (keywords,media,start,end))
-    results = json.loads(app.core.views._sentence_numfound(api_key, keywords, media, start, end))
-    return _assemble_csv_response(results, _sentence_export_props, _sentence_export_columns,'sentence-counts')
+    try:
+        results = json.loads(app.core.views._sentence_numfound(api_key, keywords, media, start, end))
+        return _assemble_csv_response(results, _sentence_export_props, _sentence_export_columns,'sentence-counts')
+    except Exception as exception:
+        return json.dumps({'error':str(exception)}, separators=(',',':')), 400
 
 def _assemble_csv_response(results,properties,column_names,filename):
     # stream back a csv
@@ -40,11 +46,17 @@ def _assemble_csv_response(results,properties,column_names,filename):
 def wordcount_csv(keywords, media, start, end):
     api_key = flask_login.current_user.get_id()
     user_mc = mcapi.MediaCloud(api_key)
-    results = json.loads(app.core.views._wordcount(user_mc, keywords, media, start, end))
-    return _assemble_csv_response(results,_wordcount_export_props,_wordcount_export_props,'wordcount')
+    try:
+        results = json.loads(app.core.views._wordcount(user_mc, keywords, media, start, end))
+        return _assemble_csv_response(results,_wordcount_export_props,_wordcount_export_props,'wordcount')
+    except Exception as exception:
+        return json.dumps({'error':str(exception)}, separators=(',',':')), 400
 
 @flapp.route('/api/demo/wordcount/<keywords>/csv')
 def demo_wordcount_csv(keywords):
     media, start, end = app.core.views.demo_params()
-    results = json.loads(app.core.views._wordcount(mc, keywords, media, start, end))
-    return _assemble_csv_response(results,_wordcount_export_props,_wordcount_export_props,'demo-wordcount')
+    try:
+        results = json.loads(app.core.views._wordcount(mc, keywords, media, start, end))
+        return _assemble_csv_response(results,_wordcount_export_props,_wordcount_export_props,'demo-wordcount')
+    except Exception as exception:
+        return json.dumps({'error':str(exception)}, separators=(',',':')), 400
