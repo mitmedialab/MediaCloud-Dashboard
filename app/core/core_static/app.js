ddf2e02c41b1f2bab1ef8232720b90c4d86936eb
Rahul B <rahulbot@gmail.com>
2018-03-21 15:59:11 -0400

    Add explorer invite banner, with magic link rewriting

--- app/core/core_static/app.js
+++ app/core/core_static/app.js
@@ -16,6 +16,54 @@ App = {
       return 'https://'+tool+'.'+App.config.cookieOpts['domain']+'/#'+App.con.queryCollection.dashboardUrl();
     },
 
+    queryToExplorer: function(){
+      var explorerUrl = "https://explorer.mediacloud.org/";
+      var hashParts = window.location.hash.split("/");
+      if (hashParts.length < 2) {
+        window.location = explorerUrl;
+      }
+      // figure out what the query is
+      var keywords = JSON.parse(decodeURIComponent(hashParts[1]));
+      var queryCount = keywords.length;
+      var sourcesAndCollections = JSON.parse(decodeURIComponent(hashParts[2]));
+      console.log(sourcesAndCollections);
+      var startDates = JSON.parse(decodeURIComponent(hashParts[3]));
+      var endDates = JSON.parse(decodeURIComponent(hashParts[4]));
+      var props = JSON.parse(decodeURIComponent(hashParts[5]));
+      var queries = [];
+      if (App.con.userModel.get('authenticated')) {
+        for(i=0;i<queryCount;i++) {
+          var parts =startDates[i].split('-');
+          var startDate = new Date(parts[0], parts[1], parts[2]).toISOString().substr(0,10);
+          parts =endDates[i].split('-');
+          var endDate = new Date(parts[0], parts[1], parts[2]).toISOString().substr(0,10);
+          queries[i] = {
+            label: encodeURIComponent(props[i].name),
+            q: encodeURIComponent(keywords[i]),
+            color: encodeURIComponent("#"+props[i].color),
+            startDate: startDate,
+            endDate: endDate,
+            sources: sourcesAndCollections[i].sources || [],
+            collections: sourcesAndCollections[i].sets || [],
+          };
+        }
+        //console.log(queries);
+        explorerUrl += "/#/queries/search?q="+JSON.stringify(queries);
+      } else {
+        for(i=0;i<queryCount;i++) {
+          queries[i] = {
+            index: i,
+            q: encodeURIComponent(keywords[i]),
+            color: encodeURIComponent("#"+props[i].color)
+          };
+        }
+        console.log(queries);
+        // demo mode:
+        explorerUrl += "/#/queries/demo/search?q="+JSON.stringify(queries);
+      }
+      window.location = explorerUrl;
+    },
+
     getSourcesUrl: function(type,id){
       // type = [ sources | collections ]
       return 'https://sources.mediacloud.org/#'+type+'/'+id;
